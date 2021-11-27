23c669b320f40fb8fad6056661283d565de83aee
Rayhan-labib7 <87138791+Rayhan-labib7@users.noreply.github.com>
2021-11-27 09:55:41 +0600

    Create num_(1).cpp

---
+++ num_(1).cpp
@@ -0,0 +1,61 @@
+//Base class function are declared as pure virtual function and write c++ code
+
+#include<bits/stdc++.h>
+using namespace std;
+class Preson
+{
+    public :
+    virtual void Set(int,string)=0;
+    virtual void Introduce()=0;
+};
+class Student
+{
+    public :
+    int Sage;
+    string Sname;
+    void Set(int Sage,string Sname)
+    {
+        this->Sage=Sage;
+        this->Sname=Sname;
+    }
+    void Introduce()
+    {
+        cout<<"Hello i am student :"<<endl;
+    }
+    void displaystudent()
+    {
+        cout<<"Age : "<<Sage<<endl<<"Name :"<<Sname<<endl;
+    }
+};
+class Teacher
+{
+    public :
+    int Tage;
+    string Tname;
+    void Set(int Tage,string Tname)
+    {
+        this->Tage=Tage;
+        this->Tname=Tname;
+    }
+    void Introduce()
+    {
+        cout<<"Hello Teachers :"<<endl;
+    }
+    void displayteacher()
+    {
+        cout<<"Age : "<<Tage<<endl<<"Name : "<<Tname<<endl;
+    }
+};
+int main()
+{
+    Student ob1;      //Student class object
+    ob1.Set(23,"Labib");
+    ob1.Introduce();
+    ob1.displaystudent();
+
+    Teacher ob2;    //Teacher class object
+    ob2.Set(35,"Respectable Teachers");
+    ob2.Introduce();
+    ob2.displayteacher();
+
+}
